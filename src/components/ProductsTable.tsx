d8caccbaa8d0b220bf4f69a235cc5b858a424ecb
lucka79 <l.batelkova@gmail.com>
2024-10-26 21:11:54 +0200

    product table + filter price

--- src/components/ProductsTable.tsx
+++ src/components/ProductsTable.tsx
@@ -33,13 +33,14 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 const columns = (categories: Category[]): ColumnDef<Product>[] => [
-  {
-    accessorKey: "created_at",
-    header: "Created",
-    cell: ({ row }) => new Date(row.original.created_at).toLocaleDateString(),
-  },
+  //   {
+  //     accessorKey: "created_at",
+  //     header: "Created",
+  //     cell: ({ row }) => new Date(row.original.created_at).toLocaleDateString(),
+  //   },
   {
     accessorKey: "id",
     header: "ID",
@@ -79,7 +80,8 @@ export function ProductsTable() {
   );
   //   const category = categories?.find((c) => c.id === products.category_id);
   const navigate = useNavigate();
-  const [categoryFilter, setCategoryFilter] = useState<string>("");
+  const [categoryFilter, setCategoryFilter] = useState<string>("all");
+  const [priceFilter, setPriceFilter] = useState<string>("all");
 
   const handleCreateProduct = () => {
     setSelectedProductId(null);
@@ -94,7 +96,9 @@ export function ProductsTable() {
     return (
       products?.filter(
         (product) =>
-          (!categoryFilter ||
+          (priceFilter === "all" ||
+            (priceFilter === "mobile" && product.priceMobil > 0)) &&
+          (categoryFilter === "all" ||
             product.category_id.toString() === categoryFilter) &&
           Object.values(product).some(
             (value) =>
@@ -106,7 +110,7 @@ export function ProductsTable() {
           )
       ) || []
     );
-  }, [products, categoryFilter, globalFilter]);
+  }, [products, categoryFilter, globalFilter, priceFilter]);
 
   const table = useReactTable({
     data: filteredProducts,
@@ -124,109 +128,124 @@ export function ProductsTable() {
   if (error) return <div>Error loading orders</div>;
 
   return (
-    <Card className="my-0 p-4">
-      <div className="space-y-4">
-        <div className="flex justify-between items-center">
-          <div className="flex gap-4">
+    <>
+      <Card className="my-0 p-4">
+        <div className="space-y-4">
+          <div className="flex justify-between items-center">
+            <div>
+              <Tabs value={categoryFilter} onValueChange={setCategoryFilter}>
+                <TabsList>
+                  <TabsTrigger value="all">All</TabsTrigger>
+                  {categories?.map((category) => (
+                    <TabsTrigger
+                      key={category.id}
+                      value={category.id.toString()}
+                    >
+                      {category.name}
+                    </TabsTrigger>
+                  ))}
+                </TabsList>
+              </Tabs>
+            </div>
+            <div>
+              <Button variant="outline" onClick={handleCreateProduct}>
+                Create Product
+              </Button>
+            </div>
+          </div>
+          <div className="flex justify-between items-center">
             <Input
               placeholder="Search product..."
               value={globalFilter}
               onChange={(e) => setGlobalFilter(e.target.value)}
               className="max-w-sm"
             />
-            <Select value={categoryFilter} onValueChange={setCategoryFilter}>
+
+            <Select value={priceFilter} onValueChange={setPriceFilter}>
               <SelectTrigger className="w-[180px]">
-                <SelectValue placeholder="Filter by category" />
+                <SelectValue placeholder="Filter by price" />
               </SelectTrigger>
               <SelectContent>
-                <SelectItem value="all">All Categories</SelectItem>
-                {categories?.map((category) => (
-                  <SelectItem key={category.id} value={category.id.toString()}>
-                    {category.name}
-                  </SelectItem>
-                ))}
+                <SelectItem value="all">All Prices</SelectItem>
+                <SelectItem value="mobile">Mobile Price {">"} 0</SelectItem>
               </SelectContent>
             </Select>
           </div>
-          <Button variant="outline" onClick={handleCreateProduct}>
-            Create Product
-          </Button>
-        </div>
-        <Table>
-          <TableHeader>
-            {table.getHeaderGroups().map((headerGroup) => (
-              <TableRow key={headerGroup.id}>
-                {headerGroup.headers.map((header) => (
-                  <TableHead key={header.id}>
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(
-                          header.column.columnDef.header,
-                          header.getContext()
-                        )}
-                  </TableHead>
-                ))}
-              </TableRow>
-            ))}
-          </TableHeader>
-          <TableBody>
-            {table.getRowModel().rows?.length ? (
-              table.getRowModel().rows.map((row) => (
-                <TableRow
-                  key={row.id}
-                  onClick={() => setSelectedProductId(row.original.id)}
-                  className="cursor-pointer hover:bg-muted/50"
-                >
-                  {row.getVisibleCells().map((cell) => (
-                    <TableCell key={cell.id}>
-                      {flexRender(
-                        cell.column.columnDef.cell,
-                        cell.getContext()
-                      )}
-                    </TableCell>
+          <Table>
+            <TableHeader>
+              {table.getHeaderGroups().map((headerGroup) => (
+                <TableRow key={headerGroup.id}>
+                  {headerGroup.headers.map((header) => (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
                   ))}
-                  <div className="flex mt-2 self-center gap-2">
-                    {/* <Badge variant="outline">{row.original.status}</Badge> */}
-                    <FileSearch2
-                      className="cursor-pointer hover:bg-muted/50"
-                      size={20}
-                      // onClick={() => setSelectedOrderId(row.original.id)}
-                      //   onClick={() => openOrderDetails(row.original.id)}
-                    />
-                  </div>
                 </TableRow>
-              ))
-            ) : (
-              <TableRow>
-                <TableCell
-                  colSpan={columns.length}
-                  className="h-24 text-center"
-                >
-                  No results.
-                </TableCell>
-              </TableRow>
-            )}
-          </TableBody>
-        </Table>
-        <div className="flex items-center justify-end space-x-2 py-4">
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.previousPage()}
-            disabled={!table.getCanPreviousPage()}
-          >
-            Previous
-          </Button>
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.nextPage()}
-            disabled={!table.getCanNextPage()}
-          >
-            Next
-          </Button>
+              ))}
+            </TableHeader>
+            <TableBody>
+              {table.getRowModel().rows?.length ? (
+                table.getRowModel().rows.map((row) => (
+                  <TableRow
+                    key={row.id}
+                    onClick={() => setSelectedProductId(row.original.id)}
+                    className="cursor-pointer hover:bg-muted/50"
+                  >
+                    {row.getVisibleCells().map((cell) => (
+                      <TableCell key={cell.id}>
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext()
+                        )}
+                      </TableCell>
+                    ))}
+                    <div className="flex mt-2 self-center gap-2">
+                      {/* <Badge variant="outline">{row.original.status}</Badge> */}
+                      {/* <FileSearch2
+                        className="cursor-pointer hover:bg-muted/50"
+                        size={20}
+                        onClick={() => setSelectedOrderId(row.original.id)}
+                      /> */}
+                    </div>
+                  </TableRow>
+                ))
+              ) : (
+                <TableRow>
+                  <TableCell
+                    colSpan={columns.length}
+                    className="h-24 text-center"
+                  >
+                    No results.
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+          <div className="flex items-center justify-end space-x-2 py-4">
+            <Button
+              variant="outline"
+              size="sm"
+              onClick={() => table.previousPage()}
+              disabled={!table.getCanPreviousPage()}
+            >
+              Previous
+            </Button>
+            <Button
+              variant="outline"
+              size="sm"
+              onClick={() => table.nextPage()}
+              disabled={!table.getCanNextPage()}
+            >
+              Next
+            </Button>
+          </div>
         </div>
-      </div>
-    </Card>
+      </Card>
+    </>
   );
 }
